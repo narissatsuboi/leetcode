b98096b991ce0db6bce94b4598e918c270091250
Narissa Tsuboi <narissatsuboi@gmail.com>
2021-09-25 18:44:33 -0700

    771
    Signed-off-by: Narissa Tsuboi <narissatsuboi@gmail.com>

---
+++ 771. Jewels and Stones/main.py
@@ -0,0 +1,40 @@
+"""
+You're given strings jewels representing the types of stones that are jewels,
+and stones representing the stones you have. Each character in stones is a
+type of stone you have. You want to know how many of the stones you have are
+also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone
+from "A".
+"""
+
+
+class Solution(object):
+    # hash set O(n) time | O(n) space
+    # for each stone, check whether it matches any of the jewels
+    def numJewelsInStones(self, jewels, stones):
+        jewel_set = set(jewels)
+        # also works ...
+        # sum(stone in jewels for stone in stones)
+        
+        return sum(s in jewel_set for s in stones)
+
+    # brute force O(n2) time | O(n) space
+    def numJewelsInStones1(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+
+        count = 0
+
+        for stone in stones:
+            if stone in jewels:
+                count += 1
+        return count
+
+
+if __name__ == '__main__':
+    jewels = "aA"
+    stones = "aAAbbbb"
